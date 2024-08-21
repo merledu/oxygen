8fd295cb8850ddfb6ec0376e618e68c08570a0e4
Abdul Rehman <22sp-052-cs@students.uituniversity.edu.pk>
2024-08-21 22:08:52 +0500

    renamed app

--- Hex_dump/apps.py
+++ Hex_dump/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HexDumpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Hex_dump'
+    name = 'hex_dump'
--- oxygen/settings.py
+++ oxygen/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Itype',
-    'Hex_dump'
+    'hex_dump'
 ]
 
 MIDDLEWARE = [
