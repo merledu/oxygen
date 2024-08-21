c16107f6d4882c67a2c169e54e03bfdbd6bf479f
mrarwho <22sp-052-cs@students.uitu.edu.pk>
2024-08-21 16:38:40 +0500

    updated settings for local access

--- oxygen/settings.py
+++ oxygen/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#-48^v%o7#duu)4awo$*91&v7n0#cyi2u@6dzk9)bz@3w$a10a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
