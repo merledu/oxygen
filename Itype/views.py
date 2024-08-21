1ff1711c7615952f33c23f9552393c866f940d68
MrARwho <22sp-052-cs@students.uituniversity.edu.pk>
2024-08-21 15:07:44 +0500

    run button updated

--- Itype/views.py
+++ Itype/views.py
@@ -83,16 +83,14 @@ def get_hex_gcc(code):
     
 
 def extract_pc_hex(filename):
-    file = None
     pc_hex_dict = {}
-    with open(filename, 'r') as file_opn:
-        file = file_opn
-    for line in file:
-        parts = line.split(':')
-        if len(parts) > 1 and parts[1].strip(): 
-            pc, hex_value = parts[0].strip(), parts[1].split()[0]
-            if (pc!='ins'):
-                pc_hex_dict[pc] = hex_value    
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.split(':')
+            if len(parts) > 1 and parts[1].strip(): 
+                pc, hex_value = parts[0].strip(), parts[1].split()[0]
+                if (pc!='ins'):
+                    pc_hex_dict[pc] = hex_value    
     return pc_hex_dict
 
 
