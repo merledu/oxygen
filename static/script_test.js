3b9ba1575a2c2f75c5b4d06ec5b2ad36568dddc7
SSaadAKHTAR <141241162+SSaadAKHTAR@users.noreply.github.com>
2024-10-22 20:24:12 +0500

    fixed info box

--- static/script_test.js
+++ static/script_test.js
@@ -523,19 +523,19 @@ document.getElementById('scrollDownBtn').addEventListener('click', () => {
  window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+// var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.scrollY >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 const openbtn = document.querySelector('.info-btn');
 const closebtn = document.querySelector('#close-btn');
