699efc1fe894d2676225ea285bc72bb724605ac4
SSaadAKHTAR <141241162+SSaadAKHTAR@users.noreply.github.com>
2025-01-03 15:03:27 +0500

    lhu  and lbu bug fixed

--- Temp/Datapath_single.py
+++ Temp/Datapath_single.py
@@ -189,8 +189,7 @@ class RISCVSimulatorSingle:
         # LHU
         elif funct3 == 0x5 and opcode == 0x3:
             address = self.registers[rs1] + self.sign_extend(imm, 12)
-            self.registers[rd] = self.sign_extend(self.memory.get(str(address, 0))| self.memory.get(str(address + 1) << 8, 0),16)
-            
+            self.registers[rd] = self.sign_extend((self.memory.get(str(address), 0) | self.memory.get(str(address + 1), 0)<<8),16)
             
             
     def execute_s_type(self, instruction):
